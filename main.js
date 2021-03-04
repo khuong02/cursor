a71b73f3015ee581724390c9f5d529a745028664
khuong02 <75919504+khuong02@users.noreply.github.com>
2021-03-05 00:50:13 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
 const cursor = document.querySelectorAll(".cursor");
-// const cursor1 = document.querySelector(".cursor1");
-console.log(cursor);
 document.addEventListener("mousemove", (i) => {
   Array.from(cursor).map((item) => {
     return cursorMove(item, i);
   });
 });
 
-function cursorMove(value, element) {
+function cursorMove(value, e) {
   value.setAttribute(
     "style",
     "top: " +
-      (element.pageY - value.offsetHeight / 2) +
+      (e.pageY - value.offsetHeight / 2) +
       "px; left: " +
-      (element.pageX - value.offsetWidth / 2) +
+      (e.pageX - value.offsetWidth / 2) +
       "px;"
   );
 }
